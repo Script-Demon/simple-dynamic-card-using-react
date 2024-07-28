53fac743c1175f0328c175b4b387a8eeaee81e48
Script-Demon <tanveermahmud0@gmail.com>
2024-07-28 11:45:10 -0700

    react-app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./components/card";
+function app() {
+
+  return <>
+  <Card cardTitle="first" buttonInfo="buy now"/>
+  <Card cardTitle="second" buttonInfo="get now"/>
+  <Card cardTitle="third" buttonInfo="pay now"/>
+  </>
+}
+
+export default app;
---
+++ src/components/card.jsx
@@ -0,0 +1,10 @@
+function Card({cardTitle,buttonInfo}) {
+
+    return <>
+    <h1>{cardTitle}</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, inventore.</p>
+    <button>{buttonInfo}</button>
+    </>
+}
+
+export default Card;
